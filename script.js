6f9cf799e82b98c9f0c67802b5a5b492b492047a
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-05-17 13:56:32 +0300

    init3

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 "use strict"
 
-// 1 užduotis
-// Jums paskambino pažinčių portalas – jiems reikia greitai sukurti front-endą, kuris pasiimtų duomenis iš https://randomuser.me/api/ ir juos atvaizduotų juos puslapyje. Duomenys galėtų būti atvaizduoti panašioje kortelėje. Svarbu atvaizduoti nuotrauką, vardą, amžių ir el. pašto adresą.
-
 fetch("https://randomuser.me/api/")
     .then(response => response.json())
     .then(data => {
